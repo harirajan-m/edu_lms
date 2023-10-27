8e2d6cf7d590b7625513f95afd921ea8687e82d3
Hari <harir@192.168.1.16>
2023-10-26 23:32:27 -0400

    refactored

--- src/main/java/com/edureka/hari/lms/controller/AuthorController.java
+++ src/main/java/com/edureka/hari/lms/controller/AuthorController.java
@@ -26,31 +26,26 @@ public class AuthorController {
     @Autowired
     AuthorService authorService;
 
-    // method to get all authors
     @GetMapping("/api/authors")
     public List<Author> getAllAuthors() {
         return authorService.getAuthors();
     }
 
-    // write a controller method to get a single author by id with GET
     @GetMapping("/api/authors/{id}")
     public Author getAuthorById(@RequestParam("id") Long id) {
         return authorService.getAuthorById(id);
     }
 
-    // write a controller method to add an author with POST
     @PostMapping("/api/authors")
     public Author addAuthor(@RequestBody Author author) {
         return authorService.addAuthor(author);
     }
 
-    // write a controller method to update an author with PUT
     @PutMapping("/api/authors/{id}")
     public Author updateAuthor(@RequestBody Author author) {
         return authorService.updateAuthor(author);
     }
 
-    // write a controller method to delete an author with DELETE
     @DeleteMapping("/api/authors/{id}")
     public void deleteAuthor(@RequestParam("id") Long id) {
         authorService.deleteAuthor(id);
